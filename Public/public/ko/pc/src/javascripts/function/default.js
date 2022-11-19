dc2dfc7fe52edd3b1bab7cf8d1a8f842986ce091
amilk5u <amilk5u@naver.com>
2022-11-20 01:57:26 +0900

    javascript 로 전환

--- Public/public/ko/pc/src/javascripts/function/default.js
+++ Public/public/ko/pc/src/javascripts/function/default.js
@@ -1,22 +1,31 @@
 "use strict";
-let winW;
-let winH;
-let $window = $(window);
-let winSc = $(window).scrollTop();
-$window.load(function () {
-   let _this = $(this);
-   winW = _this.width();
-   winH = _this.height();
-   winSc = _this.scrollTop();
-   $window.on("resize", function () {
-      winW = _this.width();
-      winH = _this.height();
+let windowWidth;
+let windowHeight;
+let windowScrollTop;
+
+function reportWindowSize() {
+   // let _this = $(this);
+   // console.log(e.currentTarget)
+
+   // winW = _this.width();
+   // winH = _this.height();
+   // windowScrollTop = _this.scrollTop();
+
+   // // _this.trigger("resize");
+   window.addEventListener('resize', (e) => {
+      // console.log(e)
+      // console.log(e.currentTarget)
+      // winW = _this.width();
+      // winH = _this.height();
    });
-   _this.trigger("resize");
-   $(window).scroll(function () {
-      winSc = _this.scrollTop();
-      $('.top').text(winSc);
+
+   window.addEventListener('scroll', (e) => {
+      windowScrollTop = window.pageYOffset;
+      document.querySelector('.top').innerHTML = windowScrollTop;
    });
+
    layout();
    main();
-});
+}
+
+window.addEventListener('load', reportWindowSize);
