750a0cbacab371faa65c356667a661e8068e2b36
amilk5u <amilk5u@naver.com>
2022-11-19 22:26:34 +0900

    default.js var -> let 수정

--- Public/public/ko/pc/dist/javascripts/design_common.js
+++ Public/public/ko/pc/dist/javascripts/design_common.js
@@ -1,10 +1,10 @@
 "use strict";
-var winW;
-var winH;
-var $window = $(window);
-var winSc = $(window).scrollTop();
+let winW;
+let winH;
+let $window = $(window);
+let winSc = $(window).scrollTop();
 $window.load(function () {
-   var _this = $(this);
+   let _this = $(this);
    winW = _this.width();
    winH = _this.height();
    winSc = _this.scrollTop();
